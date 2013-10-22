bf418512784b4dd0a81660e60becaa8f5f2c4b99
Adrian-Tudor Panescu <adrian.tudor.panescu@cern.ch>
2013-10-22 09:11:07 +0200

    deploy: backup location change

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # .jshintrc
 DOTFILES=( "bashrc" "gitconfig" "inputrc" "screenrc" "vimrc" )
 
-mkdir ~/tmp
-mkdir ~/tmp/dotfiles_backup
+mkdir ~/var
+mkdir ~/var/dotfiles_backup
 
 for file in "${DOTFILES[@]}"
 do
-	mv ~/.$file ~/tmp/dotfiles_backup/ # backup the existing dotfile
+	mv ~/.$file ~/var/dotfiles_backup/$file # backup the existing dotfile
 	ln -s $DIR/$file ~/.$file # link the new dotfile
 done
 
 # create an empty .bash_aliases file
 touch $DIR/bash_aliases
-mv ~/.bash_aliases ~/tmp/dotfiles_backup/
+mv ~/.bash_aliases ~/var/dotfiles_backup/
 ln -s $DIR/bash_aliases ~/.bash_aliases
 
 source ~/.bashrc
@@ -29,4 +29,5 @@ ln -s $DIR/diff.py ~/bin/diff.py
 chmod +x ~/bin/diff.py
 
 # ln -s jshintrc ~/.jshintrc
+# rm ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 # ln -s Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
